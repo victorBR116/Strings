bb8ef44f04803f750a822e592795643064236343
victorBR116 <65189745+victorBR116@users.noreply.github.com>
2022-09-15 23:52:23 -0300

    att

--- cadastro.php
+++ cadastro.php
@@ -17,7 +17,7 @@ $usuario = new Usuario($_POST['nome'], $_POST['senha'], $_POST['genero']);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="bootstrap.min.css">
-    <title>Curso Strings</title>
+    <title>Curso Strings Alura</title>
 </head>
 <body>
 
